4f98090a07dbfbeeb6061e22c3fbd629bec899f5
Barry Hoggard <bhoggard@gmail.com>
2025-02-17 21:07:04 -0500

    working version

--- mt/convert-textile-to-html.rb
+++ mt/convert-textile-to-html.rb
@@ -1,3 +1,32 @@
 #!/usr/bin/env ruby
 
-puts "hello world"
+require "RedCloth"
+require "sequel"
+
+# client = Mysql2::Client.new(username: "root", database: "mt")
+
+# results = client.query("SELECT * FROM mt_entry WHERE entry_convert_breaks='textile_2' limit 1")
+
+# results.each do |row|
+#   puts row["entry_id"]
+#   converted = RedCloth.new(row["entry_text"]).to_html
+
+#   # fix asset URLs for https
+#   converted = converted.gsub("http://bloggy.com", "https://bloggy.com")
+
+#   puts converted
+# end
+
+DB = Sequel.connect("mysql2://root@localhost:3306/mt")
+
+entries = DB[:mt_entry]
+
+entries.where(entry_convert_breaks: "textile_2").each do |row|
+  id = row[:entry_id]
+  puts id
+  converted = RedCloth.new(row[:entry_text]).to_html
+  # fix asset URLs for https
+  converted.gsub!("http://bloggy.com", "https://bloggy.com")
+
+  entries.where(entry_id: id).update(entry_text: converted, entry_convert_breaks: "__default__")
+end
